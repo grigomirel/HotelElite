983d557e4fb702229c954b290b3933105c78d1e8
Mirel <grigo_me15@yahoo.com>
2026-08-12 21:07:09 +0300

    Sablon_Home_Page

---
+++ css/save.py
@@ -0,0 +1,14 @@
+body {
+    padding-top: 70px; /* Required padding for .navbar-fixed-top. Change if height of navigation changes. */
+	height: 100%;
+    position: relative;
+    background-color: #C9E7EE;
+    background-image:
+        linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)),
+        url("../images/indexBackgroundImage.jpeg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    line-height:1.6em !important;
+    color:#666
+}
